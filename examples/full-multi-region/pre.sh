2f642a9c685f58f858cf4cc68656ca940584f2ab
Jared Holgate <jaredholgate@microsoft.com>
2025-03-12 10:48:14 +0000

    Fix e2e tests

--- examples/full-multi-region/pre.sh
+++ examples/full-multi-region/pre.sh
@@ -1,5 +1,3 @@
-echo "Running pre.sh script..."
-echo "Downloading the terraform.tfvars file for the hub-and-spoke-vnet example..."
-curl -o terraform.tfvars https://raw.githubusercontent.com/Azure/alz-terraform-accelerator/refs/heads/main/templates/platform_landing_zone/examples/full-multi-region/hub-and-spoke-vnet.tfvars
+echo "Downloading and creating the test.auto.tfvars file from https://raw.githubusercontent.com/Azure/alz-terraform-accelerator/refs/heads/main/templates/platform_landing_zone/examples/full-multi-region/hub-and-spoke-vnet.tfvars..."
+curl -o test.auto.tfvars https://raw.githubusercontent.com/Azure/alz-terraform-accelerator/refs/heads/main/templates/platform_landing_zone/examples/full-multi-region/hub-and-spoke-vnet.tfvars
 echo "File downloaded successfully."
-echo "Finished running pre.sh script."
