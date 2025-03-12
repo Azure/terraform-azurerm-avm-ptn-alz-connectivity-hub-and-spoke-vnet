4fc30feaf0b1832637f313528a85802b046df63f
Jared Holgate <jaredholgate@microsoft.com>
2025-03-12 10:33:16 +0000

    Add some logging

--- examples/full-multi-region/pre.sh
+++ examples/full-multi-region/pre.sh
@@ -1,1 +1,5 @@
+echo "Running pre.sh script..."
+echo "Downloading the terraform.tfvars file for the hub-and-spoke-vnet example..."
 curl -o terraform.tfvars https://raw.githubusercontent.com/Azure/alz-terraform-accelerator/refs/heads/main/templates/platform_landing_zone/examples/full-multi-region/hub-and-spoke-vnet.tfvars
+echo "File downloaded successfully."
+echo "Finished running pre.sh script."
