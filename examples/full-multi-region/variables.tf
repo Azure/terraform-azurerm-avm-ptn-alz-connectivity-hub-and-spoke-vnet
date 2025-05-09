892b66b19ca229cac06a1c8925b1f9dfb28cdd48
Jared Holgate <jaredholgate@microsoft.com>
2025-05-09 20:37:46 +0100

    grept and lint

--- examples/full-multi-region/variables.tf
+++ examples/full-multi-region/variables.tf
@@ -1,3 +1,20 @@
+variable "connectivity_resource_groups" {
+  type = map(object({
+    name     = string
+    location = string
+  }))
+  default     = {}
+  description = <<DESCRIPTION
+A map of resource groups to create. These must be created before the connectivity module is applied.
+
+The following attributes are supported:
+
+  - name: The name of the resource group
+  - location: The location of the resource group
+
+DESCRIPTION
+}
+
 variable "connectivity_type" {
   type        = string
   default     = "hub_and_spoke_vnet"
@@ -24,42 +41,6 @@ variable "enable_telemetry" {
   description = "Flag to enable/disable telemetry"
 }
 
-variable "management_group_settings" {
-  type        = any
-  default     = {}
-  description = <<DESCRIPTION
-The settings for the management groups. Details of the settings can be found in the module documentation at https://registry.terraform.io/modules/Azure/avm-ptn-alz
-DESCRIPTION
-}
-
-variable "management_resource_settings" {
-  type        = any
-  default     = {}
-  description = <<DESCRIPTION
-The settings for the management resources. Details of the settings can be found in the module documentation at https://registry.terraform.io/modules/Azure/avm-ptn-alz-management
-DESCRIPTION
-}
-variable "starter_locations" {
-  type        = list(string)
-  description = "The default for Azure resources. (e.g 'uksouth')"
-  default     = ["uksouth", "ukwest"]
-}
-variable "connectivity_resource_groups" {
-  type = map(object({
-    name     = string
-    location = string
-  }))
-  default     = {}
-  description = <<DESCRIPTION
-A map of resource groups to create. These must be created before the connectivity module is applied.
-
-The following attributes are supported:
-
-  - name: The name of the resource group
-  - location: The location of the resource group
-
-DESCRIPTION
-}
 variable "hub_and_spoke_vnet_settings" {
   type        = any
   default     = {}
@@ -72,6 +53,7 @@ The following attributes are supported:
 
 DESCRIPTION
 }
+
 variable "hub_and_spoke_vnet_virtual_networks" {
   type = map(object({
     hub_virtual_network = any
@@ -96,6 +78,29 @@ The following attributes are supported:
 
 DESCRIPTION
 }
+
+variable "management_group_settings" {
+  type        = any
+  default     = {}
+  description = <<DESCRIPTION
+The settings for the management groups. Details of the settings can be found in the module documentation at https://registry.terraform.io/modules/Azure/avm-ptn-alz
+DESCRIPTION
+}
+
+variable "management_resource_settings" {
+  type        = any
+  default     = {}
+  description = <<DESCRIPTION
+The settings for the management resources. Details of the settings can be found in the module documentation at https://registry.terraform.io/modules/Azure/avm-ptn-alz-management
+DESCRIPTION
+}
+
+variable "starter_locations" {
+  type        = list(string)
+  default     = ["uksouth", "ukwest"]
+  description = "The default for Azure resources. (e.g 'uksouth')"
+}
+
 variable "tags" {
   type        = map(string)
   default     = null
