e1a3cd4300439da7cb154f74f3145e21d197d7df
Jared Holgate <jaredholgate@microsoft.com>
2025-05-30 15:19:58 +0100

    feat: dns updates

--- examples/full-multi-region/outputs.tf
+++ examples/full-multi-region/outputs.tf
@@ -3,3 +3,11 @@ output "linting" {
     connectivity_type = var.connectivity_type
   }
 }
+
+output "test_outputs" {
+  value = module.test
+}
+
+output "config_outputs" {
+  value = module.config
+}
