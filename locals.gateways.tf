ffdfbe3ddbf6d9da39d90d77f11cb2430f2dbb59
Jared Holgate <jaredholgate@microsoft.com>
2025-10-03 07:24:49 +0100

    linting

--- locals.gateways.tf
+++ locals.gateways.tf
@@ -1,6 +1,5 @@
 locals {
-  resource_group_resource_type = "Microsoft.Resources/resourceGroups"
-  virtual_network_gateways     = merge(local.virtual_network_gateways_express_route, local.virtual_network_gateways_vpn)
+  virtual_network_gateways = merge(local.virtual_network_gateways_express_route, local.virtual_network_gateways_vpn)
   virtual_network_gateways_express_route = {
     for hub_network_key, hub_network_value in var.hub_virtual_networks : "${hub_network_key}-express-route" => {
       virtual_network_gateway_subnet_id = module.hub_and_spoke_vnet.virtual_networks[hub_network_key].subnets["${hub_network_key}-gateway"].resource_id
