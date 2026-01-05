3e52112b464abcfd1cc29606725a401218a4f119
Morten Lerudjordet <github@lerun.info>
2026-01-05 09:21:36 +0100

    remove uncomment code

--- locals.route_tables.tf
+++ locals.route_tables.tf
@@ -6,7 +6,6 @@ locals {
     bgp_route_propagation_enabled = value.virtual_network_gateways.route_table_bgp_route_propagation_enabled
     routes                        = merge(local.gateway_route_table_routes[key], local.gateway_route_table_default_route[key])
     subnet_resource_ids           = can(module.hub_and_spoke_vnet.virtual_networks[key].subnets["${key}-gateway"].resource_id) ? { gw-subnet = module.hub_and_spoke_vnet.virtual_networks[key].subnets["${key}-gateway"].resource_id } : {}
-    # subnet_resource_ids = can(module.virtual_network_gateway.subnet.id) ? { gw-subnet = module.virtual_network_gateway.subnet.id } : {}
     } if local.gateway_route_table_enabled[key]
   }
   gateway_route_table_default_route = { for key, value in var.hub_virtual_networks : key => {
