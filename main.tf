d660545d296d47429bdcea1d85922c53aa2240bf
Morten Lerudjordet <github@lerun.info>
2026-01-07 11:17:25 +0100

    depend on gw finished

--- main.tf
+++ main.tf
@@ -62,6 +62,9 @@ module "gateway_route_table" {
   routes                        = each.value.routes
   subnet_resource_ids           = each.value.subnet_resource_ids
   tags                          = var.tags
+
+  # to stop api contention on gw subnet make sure route table is created after gw is finished
+  depends_on = [virtual_network_gateway]
 }
 
 module "dns_resolver" {
