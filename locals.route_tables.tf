1e31567519dfd84b79bb5007e487fae82e1f9192
mortenlerudjordet <github@lerun.info>
2026-01-08 08:28:25 +0100

    add enabled check for gw route map creation

--- locals.route_tables.tf
+++ locals.route_tables.tf
@@ -20,12 +20,13 @@ locals {
   gateway_route_table_default_route = { for key, value in var.hub_virtual_networks : "${key}-route-gw-fw" => {
     virtual_network_key    = key
     key                    = "${key}-route-gw-fw"
+    enabled                = value.virtual_network_gateways.route_table_gateway_firewall_route_enabled
     resource_group_name    = local.hub_virtual_networks_resource_group_names[key]
     name                   = "${key}-${replace(value.virtual_network_gateways.subnet_address_prefix, "/", "-")}"
     address_prefix         = value.virtual_network_gateways.subnet_address_prefix
     next_hop_type          = "VirtualAppliance"
     next_hop_in_ip_address = try(module.hub_and_spoke_vnet.firewalls[key].private_ip_address, null)
-    } if value.virtual_network_gateways.route_table_gateway_firewall_route_enabled
+    } if value.virtual_network_gateways.route_table_gateway_firewall_route_enabled && value.virtual_network_gateways.route_table_creation_enabled
   }
   #   gateway_route_table_custom_routes = { for key, value in var.hub_virtual_networks : key => {
   #     for routeKey, route in value.virtual_network_gateways.routes : routeKey => {
@@ -42,6 +43,7 @@ locals {
         for key_rt, value_rt in value.virtual_network_gateways.routes : {
           virtual_network_key    = key
           key                    = key_rt
+          enabled                = value.virtual_network_gateways.route_table_creation_enabled
           resource_group_name    = local.hub_virtual_networks_resource_group_names[key]
           name                   = try(value_rt.name, null) != null ? value_rt.name : "${key}-${key_rt}-${replace(value_rt.address_prefix, "/", "-")}"
           address_prefix         = value_rt.address_prefix
@@ -49,7 +51,7 @@ locals {
           next_hop_in_ip_address = try(value_rt.next_hop_in_ip_address, null) != null ? value_rt.next_hop_in_ip_address : try(module.hub_and_spoke_vnet.firewalls[key].private_ip_address, null)
         }
       ]
-    ]) : route.key => route if local.gateway_route_table_enabled[route.virtual_network_key]
+    ]) : route.key => route if route.enabled
   }
   gateway_route_table_routes = merge(local.gateway_route_table_custom_routes, local.gateway_route_table_default_route)
 }
