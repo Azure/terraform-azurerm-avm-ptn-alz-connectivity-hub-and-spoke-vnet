e01fab96e64539c6a693206f9dbc60b8d0b191bc
Rishabh Aggarwal <31319270+altruistcoder@users.noreply.github.com>
2026-01-30 22:54:37 +0530

    feat: Add Domain Name Label support for Azure Firewall public IPs (#119)
    * Added support to configure the domain name labels on all the public IPs of the firewall
    
    * Updated documentation for all firewall public IP configuration sections for domain name label changes
    
    * Added the missing configuration for setting domain name labels in the variables of the hub mesh module

--- variables.tf
+++ variables.tf
@@ -219,6 +219,7 @@ variable "hub_virtual_networks" {
           sku_tier            = optional(string, "Regional")
           zones               = optional(set(string))
           public_ip_prefix_id = optional(string)
+          domain_name_label   = optional(string)
         }), {})
       }), {})
 
@@ -232,6 +233,7 @@ variable "hub_virtual_networks" {
           sku_tier            = optional(string, "Regional")
           zones               = optional(set(string))
           public_ip_prefix_id = optional(string)
+          domain_name_label   = optional(string)
         }), {})
       })), {})
 
@@ -244,6 +246,7 @@ variable "hub_virtual_networks" {
           sku_tier            = optional(string, "Regional")
           zones               = optional(set(string))
           public_ip_prefix_id = optional(string)
+          domain_name_label   = optional(string)
         }), {})
       }), {})
     }), {})
@@ -844,6 +847,7 @@ The following top level attributes are supported:
       - `zones` - (Optional) A list of availability zones to use for the public IP configuration. If not specified will be `null`.
       - `ip_version` - (Optional) The IP version to use for the public IP configuration. Possible values include `IPv4`, `IPv6`. If not specified will be `IPv4`.
       - `sku_tier` - (Optional) The SKU tier to use for the public IP configuration. Possible values include `Regional`, `Global`. If not specified will be `Regional`.
+      - `domain_name_label` - (Optional) The domain name label for the public IP configuration.
       - `public_ip_prefix_id` - (Optional) The ID of the public IP prefix.
   - `ip_configurations` - (Optional) A map of the default IP configuration for the Azure Firewall. If not specified the defaults below will be used:
     - `name` - (Optional) The name of the default IP configuration. If not specified will use `default`.
@@ -854,6 +858,7 @@ The following top level attributes are supported:
       - `zones` - (Optional) A list of availability zones to use for the public IP configuration. If not specified will be `null`.
       - `ip_version` - (Optional) The IP version to use for the public IP configuration. Possible values include `IPv4`, `IPv6`. If not specified will be `IPv4`.
       - `sku_tier` - (Optional) The SKU tier to use for the public IP configuration. Possible values include `Regional`, `Global`. If not specified will be `Regional`.
+      - `domain_name_label` - (Optional) The domain name label for the public IP configuration.
       - `public_ip_prefix_id` - (Optional) The ID of the public IP prefix.
   - `management_ip_configuration` - (Optional) An object with the following fields. If not specified the defaults below will be used:
     - `name` - (Optional) The name of the management IP configuration. If not specified will use `defaultMgmt`.
@@ -863,6 +868,7 @@ The following top level attributes are supported:
       - `zones` - (Optional) A list of availability zones to use for the public IP configuration. If not specified will be `null`.
       - `ip_version` - (Optional) The IP version to use for the public IP configuration. Possible values include `IPv4`, `IPv6`. If not specified will be `IPv4`.
       - `sku_tier` - (Optional) The SKU tier to use for the public IP configuration. Possible values include `Regional`, `Global`. If not specified will be `Regional`.
+      - `domain_name_label` - (Optional) The domain name label for the public IP configuration.
       - `public_ip_prefix_id` - (Optional) The ID of the public IP prefix.
 
 ## Azure Firewall Policy
